2801358b224880a0e1e582bc75a708c387be3712
mahmud <mahmud120398@gmail.com>
2023-02-27 16:37:41 +0700

    refactor: update logic handling scan qr code

--- resources/js/custom-scanner.js
+++ resources/js/custom-scanner.js
@@ -2,7 +2,7 @@ import { Html5Qrcode } from "html5-qrcode";
 import Sweal from "sweetalert2";
 
 const showInformation = (title, message) => {
-    Sweal.hideLoading();
+    if (Sweal.isLoading) Sweal.hideLoading();
     Sweal.fire({
         title: title,
         text: message,
@@ -12,9 +12,40 @@ const showInformation = (title, message) => {
     });
 };
 
+const getCameraAvailability = async () => {
+    try {
+        let cameras = await Html5Qrcode.getCameras();
+        return cameras;
+    } catch (e) {
+        console.log(e);
+        return null;
+    }
+};
+
+const startQrScanner = async (html5QrCode, deviceId) => {
+    try {
+        let facingMode = { deviceId: deviceId };
+
+        await html5QrCode.start(facingMode, config, (text, object) => {
+            if (html5QrCode.isScanning) {
+                html5QrCode.pause();
+                window.localStorage.setItem("qr-result", text);
+                showScanSuccess(html5QrCode);
+            }
+        });
+
+        showInformation(
+            "Scan Qr Code",
+            "Pastikan qr code terlihat dengan jelas, didalam kotak dan tidak buram"
+        );
+    } catch (e) {
+        return null;
+    }
+};
+
 const stopQrScanner = (html5QrCode) => {
-    html5QrCode.stop();
-    $("#stop").toggleClass("d-none");
+    if (html5QrCode.isScanning) html5QrCode.stop();
+    $("#btn-action").toggleClass("d-none");
     $("#start").toggleClass("d-none");
     $(".content-information").toggleClass("d-none");
 };
@@ -30,7 +61,8 @@ const showScanSuccess = (html5QrCode) => {
         allowOutsideClick: false,
     }).then((res) => {
         if (res.isConfirmed) {
-            stopQrScanner(html5QrCode);
+            // stopQrScanner(html5QrCode);
+            window.location.href = "/scan-result";
         }
     });
 };
@@ -41,37 +73,86 @@ const config = {
     aspectRatio: 1.7777778,
 };
 
-const facingMode = { facingMode: { exact: "environment" } };
+const showAvailableCameras = async (cameras) => {
+    const result = await Sweal.fire({
+        title: "Select Camera",
+        input: "select",
+        inputOptions: cameras,
+    });
+    return result.value;
+};
 
 $(function () {
     const html5QrCode = new Html5Qrcode("render", false);
 
     $("#start").on("click", () => {
         Sweal.showLoading();
-        setTimeout(() => {
-            const qrCodeSuccessCallback = (decodedText, decodedResult) => {
-                if (html5QrCode.isScanning) {
-                    html5QrCode.pause();
-                    showScanSuccess(html5QrCode);
-                }
-            };
+        window.localStorage.removeItem("qr-result");
 
-            $("#stop").toggleClass("d-none");
-            $("#start").toggleClass("d-none");
-            $(".content-information").toggleClass("d-none");
+        getCameraAvailability().then((cameras) => {
+            if (cameras && cameras.length) {
+                let selectedCamera = {};
+                cameras.forEach(
+                    (item) => (selectedCamera[item.id] = item.label)
+                );
+                showAvailableCameras(selectedCamera).then((cameraId) => {
+                    $("#start").toggleClass("d-none");
+                    $(".content-information").toggleClass("d-none");
 
-            html5QrCode
-                .start(facingMode, config, qrCodeSuccessCallback)
-                .then((_) => {
-                    showInformation(
-                        "Scan Qr Code",
-                        "Pastikan qr code terlihat dengan jelas, didalam kotak dan tidak buram"
-                    );
+                    startQrScanner(html5QrCode, cameraId).then((_) => {
+                        $("#btn-action").toggleClass("d-none");
+                    });
                 });
-        }, 3000);
+            } else {
+                showInformation("Qr Code Failure", "No camera detected!");
+            }
+        });
     });
 
     $("#stop").on("click", () => {
         stopQrScanner(html5QrCode);
+    });
+
+    $("#upload").on("click", async () => {
+        /**
+         *
+         */
+        const { value: file } = await Sweal.fire({
+            title: "Select image",
+            input: "file",
+            inputAttributes: {
+                accept: "image/*",
+                "aria-label": "Upload your profile picture",
+            },
+        });
+
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                Sweal.fire({
+                    imageUrl: e.target.result,
+                    imageAlt: "The uploaded picture",
+                    allowOutsideClick: false,
+                    showConfirmButton: true,
+                    showCancelButton: true,
+                }).then(async (res) => {
+                    if (res.isConfirmed) {
+                        try {
+                            await html5QrCode.stop();
+                            const result = await html5QrCode.scanFileV2(file);
+                            window.localStorage.setItem(
+                                "qr-result",
+                                result.decodedText
+                            );
+                            showScanSuccess(html5QrCode);
+                        } catch (e) {
+                            console.log(e);
+                            alert(e);
+                        }
+                    }
+                });
+            };
+            reader.readAsDataURL(file);
+        }
     });
 });
--- resources/views/custom-qr-scanner.blade.php
+++ resources/views/custom-qr-scanner.blade.php
@@ -24,10 +24,10 @@
             <div id="content-qr">
                 <div id="render"></div>
                 <div class="content-information">
-                    <div class="d-flex justify-content-center align-content-center flex-column p-3">
-                        <i class="fa-regular fa-qrcode"></i>
-                        <h3 class="text-center">Scan Qr Code</h3>
-                        <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. A nemo atque optio,
+                    <div class="d-flex justify-content-center align-content-center flex-column p-3 text-center">
+                        <i class="fa fa-10x fa-qrcode" style="font-size: 58px;"></i>
+                        <h3>Scan Qr Code</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A nemo atque optio,
                             ab ad
                             expedita officia
                             modi nesciunt placeat voluptatibus officiis nulla inventore. Cupiditate, perferendis!
@@ -39,7 +39,16 @@
             <div class="btn-content">
                 <div class="d-grid gap-2 d-block p-4">
                     <button class="btn btn-primary btn-sm btn-block p-2" id="start">Start</button>
-                    <button class="btn btn-danger btn-sm btn-block d-none p-2" id="stop">Stop</button>
+                    <div class="container d-none" id="btn-action">
+                        <div class="row">
+                            <div class="col-6 col-sm-6">
+                                <button class="btn btn-danger btn-sm btn-block p-2 w-100" id="stop">Stop</button>
+                            </div>
+                            <div class="col-6 col-sm-6">
+                                <button class="btn btn-info btn-sm btn-block p-2 w-100" id="upload">Select File</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
