0d3ad84adcb5f7c120978f527a7d0f34ba2cb3a3
mahmud <mahmud120398@gmail.com>
2023-02-27 16:57:15 +0700

    fix: select2 not a function

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,5 +7,7 @@ import selet2 from "select2";
 import "bootstrap";
 
 window.$ = Jquery;
-window.$.Datatable = dt;
+window.jQuery = Jquery;
+window.$.DataTable = dt;
 window.$.select2 = selet2;
+selet2();
