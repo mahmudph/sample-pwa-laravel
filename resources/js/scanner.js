a18ce4a18c7b85a3f230e29080487c9cbe97acaa
mahmud <mahmud120398@gmail.com>
2023-03-07 13:23:19 +0700

    feat: add alert sweat alert2

--- resources/js/scanner.js
+++ resources/js/scanner.js
@@ -1,4 +1,16 @@
 import QrScannerHandler from "./qr_scanner";
+import Sweal from "sweetalert2";
+
+const showInformation = async (title, message, icon = "info") => {
+    if (Sweal.isLoading) Sweal.hideLoading();
+    return Sweal.fire({
+        title: title,
+        text: message,
+        showConfirmButton: true,
+        icon: icon,
+        allowOutsideClick: false,
+    });
+};
 
 $(function () {
     let qrScannerOptions = {
@@ -20,10 +32,42 @@ $(function () {
              * do action when type event is a onReceived
              */
             if (result.type == "onReceived") {
+                showInformation("qr code scanner berhasil");
                 console.log("save data into local storage => " + result.data);
                 window.localStorage.setItem("qr-result", result.data);
             }
         });
         qrScannerHanlder.getAvailableCameraAndBindToHtml();
+    });
+
+    $("#form").on("submit", function (e) {
+        e.preventDefault();
+        console.log($(this).serialize());
+        $.ajax({
+            url: "api/master/create",
+            type: "post",
+            data: $(this).serialize(),
+            header: {
+                "X-CSRF-Token": $('meta[name="csrf-token"]').attr("content"),
+            },
+            success: async (e) => {
+                let result = await showInformation(
+                    "Add New Data",
+                    "create new ata berhasil",
+                    "success"
+                );
+                if (result.isConfirmed) {
+                    window.location.href = "/home";
+                }
+            },
+            error: (e) => {
+                console.log(e);
+                showInformation(
+                    "Add New Data",
+                    "create new ata gagal!",
+                    "error"
+                );
+            },
+        });
     });
 });
